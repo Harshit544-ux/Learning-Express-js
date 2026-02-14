4c5963a6bc3823e8e7253e23434335b1ccc8a436
Harshit <srivastava0798@gmail.com>
2026-02-14 21:24:40 +0530

    Perform CRUD in cart , write the api for which add the product to user cart

--- Day6/third.js
+++ Day6/third.js
@@ -5,14 +5,50 @@ const app = express();
 app.use(express.json());
 
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
+
+//CRUD operations in Product
+
+// Mock Product Data
+const products =[
+  { id: 1, name: "Laptop", price: 50000 },
+  { id: 2, name: "Mobile", price: 20000 },
+  { id: 3, name: "Headphones", price: 2000 }
+
+]
+
+
+//Empty Cart (User ka cart)
+let cart =[];
+
+
+//Route: POST /cart
+app.post("/cart",(req,res)=>{
+     const {productId} =req.body;
+
+     //product dhundho
+     const product = products.find(p=>p.id === productId);
+
+     if(!product){
+        return res.status(404).json({message:"Product not found"})
+     }
+
+     //cart mein add kro
+     cart.push(product);
+
+     res.json({
+        message:"Product added to cart",
+        cart
+     })
 })
+
+
+
+
 
 
 //Port
 const PORT=4000;
 
 app.listen(PORT,()=>{
-    console.log('app is listening on port 4000')
+    console.log(`app is listening on port ${PORT}`)
 })
