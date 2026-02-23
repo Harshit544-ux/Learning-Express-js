ae167fdda794967b1b695692faf9276cc31e1248
Harshit <srivastava0798@gmail.com>
2026-02-23 20:28:19 +0530

    refactor the userRouter and remove unnecessary code which used in this file

--- Day7/routes/user.js
+++ Day7/routes/user.js
@@ -1,108 +1,43 @@
 const express = require("express");
 const User = require("../model/userModal");
-const bcrypt = require("bcrypt");
-const validateUser = require("../utils/validateUser");
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET, SALT_ROUNDS } = require("../utils/constants");
+const auth = require("../middlewares/auth");
 
+//userRouter
 const userRouter=express.Router();
 
-// Feed the user data in Db
- userRouter.post("/register", async (req, res) => {
-    const { username, email, password } = req.body;
-    console.log(username, email, password);
-
+//fetch the users from  DB
+ userRouter.get("/", auth, async (req, res) => {
     try {
-        //call the validateUser function to check for mandatory fields
-        validateUser(req.body);
-
-        //Hashing the password before saving to Db
-        req.body.password = await bcrypt.hash(req.body.password, SALT_ROUNDS);
-        await User.create(req.body);
-        res.send("User Regstered Successfully");
+        const user = await User.find();
+        res.send(user);
     } catch (err) {
-        res.send("Error in user registetaion :" + err.message);
+        res.send("Error in getting info : " + err.message);
     }
 });
 
 
-
- userRouter.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    console.log(email, password);
-
+//Update user data in DB
+ userRouter.put("/", auth, async (req, res) => {
+    const { id, ...update } = req.body;
     try {
-        //Find user by email in DB
-        const userEmail = await User.findOne({ email });
-        console.log(userEmail);
-
-        //if user not found 
-        if (!userEmail) {
-            throw new Error("User not found with this email");
-        }
-
-        //compare the password
-        const isPasswordMatch = await bcrypt.compare(password, userEmail.password);
-
-        //password match nahi hua 
-        if (!isPasswordMatch) {
-            throw new Error("Invalid password");
-        }
-
-        //generate the token
-        const token = jwt.sign(
-            { id: userEmail._id, email: userEmail.email },
-            JWT_SECRET, { expiresIn: "2 days" }
-        );
-        console.log(token);
-
-        //send cookie
-        res.cookie("token", token);
-        res.send("User logged in Successfully");
+        const updatedUser = await User.findByIdAndUpdate(id, update);
+        res.send(updatedUser);
     } catch (err) {
-        res.send("Error in user login :" + err.message);
+        res.send("Error in updating user : " + err.message);
     }
 });
 
 
-//TODO  add routes for below operations with auth middleware and change naming convention of routes as per REST API standards
-
-
-//  userRouter.get("/users", auth, async (req, res) => {
-//     try {
-//         const user = await User.find();
-//         res.send(user);
-//     } catch (err) {
-//         res.send("Error in getting info : " + err.message);
-//     }
-// });
-
-
-
-// //Update user data in DB
-//  userRouter.put("/users", auth, async (req, res) => {
-//     const { id, ...update } = req.body;
-//     try {
-//         const updatedUser = await User.findByIdAndUpdate(id, update);
-//         res.send(updatedUser);
-//     } catch (err) {
-//         res.send("Error in updating user : " + err.message);
-//     }
-// });
-
-
-
-
-// //Delete user from DB
-//  userRouter.delete("/users/:id", auth, async (req, res) => {
-//     const { id } = req.params;
-//     try {
-//         const deletedUser = await User.findByIdAndDelete(id);
-//         res.send(deletedUser);
-//     } catch (err) {
-//         res.send("Error in deleting user : " + err.message);
-//     }
-// });
+//Delete user from DB
+ userRouter.delete("/:id", auth, async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedUser = await User.findByIdAndDelete(id);
+        res.send(deletedUser);
+    } catch (err) {
+        res.send("Error in deleting user : " + err.message);
+    }
+});
 
 
 module.exports = userRouter;
