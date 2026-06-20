50cf99e5fb9577bbf7430a59239c4ee89b0b17f5
Harshit <srivastava0798@gmail.com>
2026-06-20 13:28:19 +0530

    feat: add  Notes API with DELETE /notes/:index endpoint

--- server.js
+++ server.js
@@ -9,19 +9,38 @@ notes = [] //create the notes array
 // POST  /notes
 app.post("/notes",(req,res)=>{
     console.log(req.body)
+    //insert the data (req.body) into the array notes
     notes.push(req.body)
 
-    res.status(201).send({
-        message : "notes created "
+    //send the response
+    res.status(201).json({
+        message : "notes created successfully"
     })
 })
 
 
-//GET /notes
+// GET /notes
 app.get("/notes",(req,res)=>{
-    res.status(200).send({
+    //send the response
+    res.status(200).json({
         message:"notes fetch successfully",
         notes:notes
+    })
+})
+
+
+// DELETE /notes/1
+app.delete("/notes/:index",(req,res)=>{
+    // extract the index from req.params
+    const index = req.params.index
+    console.log("index",index)
+
+    //delete the notes in particular index
+    delete notes[index]
+
+    //send the response
+    res.status(200).json({
+        message : "delete successfully"
     })
 })
 
