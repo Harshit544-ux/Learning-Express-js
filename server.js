744f739b3c2b4887fe6a225152a20f7e4a21fa74
Harshit <srivastava0798@gmail.com>
2026-06-20 14:23:19 +0530

    feat: add  Notes API with PATCH  /notes/:index endpoint and refactor the file also

--- server.js
+++ server.js
@@ -1,50 +1,6 @@
 //import the app instance from app.js
 const app = require("./src/app")
 
-//Task : Notes Api
-
-notes = [] //create the notes array
-
-// title , description 
-// POST  /notes
-app.post("/notes",(req,res)=>{
-    console.log(req.body)
-    //insert the data (req.body) into the array notes
-    notes.push(req.body)
-
-    //send the response
-    res.status(201).json({
-        message : "notes created successfully"
-    })
-})
-
-
-// GET /notes
-app.get("/notes",(req,res)=>{
-    //send the response
-    res.status(200).json({
-        message:"notes fetch successfully",
-        notes:notes
-    })
-})
-
-
-// DELETE /notes/1
-app.delete("/notes/:index",(req,res)=>{
-    // extract the index from req.params
-    const index = req.params.index
-    console.log("index",index)
-
-    //delete the notes in particular index
-    delete notes[index]
-
-    //send the response
-    res.status(200).json({
-        message : "delete successfully"
-    })
-})
-
-
 //start the server
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
--- src/app.js
+++ src/app.js
@@ -6,5 +6,66 @@ const app = express() //create an instance of express
 app.use(express.json()) //middleware express.json()
 
 
+//Task : Notes Api
+
+notes = [] //create the notes array
+
+// title , description 
+// POST  /notes
+app.post("/notes",(req,res)=>{
+    console.log(req.body)
+    //insert the data (req.body) into the array notes
+    notes.push(req.body)
+
+    //send the response
+    res.status(201).json({
+        message : "notes created successfully"
+    })
+})
+
+
+// GET /notes
+app.get("/notes",(req,res)=>{
+    //send the response
+    res.status(200).json({
+        message:"notes fetch successfully",
+        notes:notes
+    })
+})
+
+
+// DELETE /notes/1
+app.delete("/notes/:index",(req,res)=>{
+    // extract the index from req.params
+    const index = req.params.index
+    console.log("index",index)
+
+    //delete the notes in particular index
+    delete notes[index]
+
+    //send the response
+    res.status(200).json({
+        message : "delete successfully"
+    })
+})
+
+
+//PATCH /notes/1
+app.patch("/notes/:id",(req,res)=>{
+    //extract index
+    const index = req.params.id
+
+    //take description from fronend
+    const description = req.body.description
+
+    //update the description on particular index
+    notes[index].description = description
+
+    res.status(200).json({
+       message: "note updated successfullt"
+    })
+
+
+})
 
 module.exports = app //export the app instance to be used in other files
