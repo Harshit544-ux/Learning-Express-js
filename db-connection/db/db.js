9dc68f0d9f26ae6a7dd6cc803e518135e420aed1
Harshit <srivastava0798@gmail.com>
2026-06-22 18:41:47 +0530

    remove the hardcoded url

--- db-connection/db/db.js
+++ db-connection/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 async function connectDB(){
-   await mongoose.connect("mongodb+srv://yt:bXMDldQxXdOg8l5N@yt-complete-backend.cqehzeu.mongodb.net/harshit")
+   await mongoose.connect(process.env.MONGO_URL)
    console.log("Connect to DB")
 }
 
